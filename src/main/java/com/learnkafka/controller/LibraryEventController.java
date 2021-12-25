fb19a0d5ef25c4e64b631b6f7049acf58f9ccd72
Leonardo T. Fernandes <leo.80@protonmail.com>
2021-12-25 12:32:27 -0300

    add LibraryEventType enum

--- src/main/java/com/learnkafka/controller/LibraryEventController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventController.java
@@ -2,6 +2,7 @@ package com.learnkafka.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.domain.LibraryEventType;
 import com.learnkafka.producer.LibraryEventProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +27,9 @@ public class LibraryEventController {
         log.info("before sendLibraryEvent");
 //        libraryEventProducer.sendLibraryEventAsynchronous_Approach1(libraryEvent);
 //        SendResult<Integer, String> sendResult = libraryEventProducer.sendLibraryEventSynchronous(libraryEvent);
-        libraryEventProducer.sendLibraryEventAsynchronous_Approach2(libraryEvent);
 //        log.info("SendResult is {}", sendResult.toString());
+        libraryEvent.setLibraryEventType(LibraryEventType.NEW);
+        libraryEventProducer.sendLibraryEventAsynchronous_Approach2(libraryEvent);
         log.info("after sendLibraryEvent");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
